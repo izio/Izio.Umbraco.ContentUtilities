11bb213a34060c79ef2e511edd5bcdd307271e2f
Patrick Robin <hello@patrickrobin.co.uk>
2016-12-12 22:11:50 +0000

    Added method definitions for deploying content types

--- Izio.Umbraco.ContentUtilities/ContentTypeCreator.cs
+++ Izio.Umbraco.ContentUtilities/ContentTypeCreator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Izio.Umbraco.ContentUtilities
 {
@@ -11,6 +12,24 @@ namespace Izio.Umbraco.ContentUtilities
     /// </summary>
     public static class ContentTypeCreator
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        public static void Deploy(string path)
+        {
+            var document = XDocument.Load("path");
 
+            Deploy(document);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        public static void Deploy(XDocument configuration)
+        {
+            
+        }
     }
 }
