5500a55e5c87075329ae4b075e4c8ac463ec5a4f
Patrick Robin <hello@patrickrobin.co.uk>
2016-12-21 19:07:14 +0000

    Added TemplateCreator framework

---
+++ Izio.Umbraco.ContentUtilities/TemplateCreator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace Izio.Umbraco.ContentUtilities
+{
+    public class TemplateCreator
+    {
+        private readonly IFileService _fileService;
+
+        public TemplateCreator()
+        {
+            _fileService = ApplicationContext.Current.Services.FileService;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        public void Deploy(string path)
+        {
+            var document = XDocument.Load(path);
+
+            Deploy(document);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        public void Deploy(XDocument configuration)
+        {
+
+            
+        }
+    }
+}
