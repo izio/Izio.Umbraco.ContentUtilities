f4a8904519bdcc44673df4b46242e197ec844b23
Patrick Robin <hello@patrickrobin.co.uk>
2017-01-09 22:09:11 +0000

    Created ICreator interface

---
+++ Izio.Umbraco.ContentUtilities/Interfaces/ICreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+namespace Izio.Umbraco.ContentUtilities.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICreator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void Deploy(XDocument configuration);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Retract(XDocument configuration);
+    }
+}
